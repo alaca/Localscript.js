0c1d1bf3b8aa94501552715df6c76524690cd0de
alaca <ante.laca@gmail.com>
2018-01-06 10:43:10 +0100

    Added indirect eval call to execute scripts in global scope

--- src/localscript.js
+++ src/localscript.js
@@ -6,46 +6,6 @@
         ls = w.localStorage,
         self = d.currentScript || d.querySelector('script[data-expire]');
 
-    d.addEventListener('DOMContentLoaded', function(){
-
-        var data = [],
-            scripts = d.getElementsByTagName('script');
-
-        // check if local storage is supported
-        if (ls) {
-            var time = Math.floor(new Date().getTime() / 1000),
-                expire = ls.getItem('ls-expire'),
-                expire_in = self.getAttribute('data-expire') || 3600  // expire in seconds
-            // clear if data is expired
-            if (null === expire || time > expire) {
-                ls.clear();
-                ls.setItem('ls-expire', parseInt(time) + parseInt(expire_in));
-            }
-        }
-
-        // process all script tags
-        for(var i in scripts) {
-            if (scripts[i].type == 'text/localscript') {
-                data.push({
-                    url : scripts[i].getAttribute('data-src') || null,
-                    code : scripts[i].innerHTML || null
-                });
-            }
-        }
-
-        // run
-        get(data).then(function(codes){
-            for(var i in codes) {
-                try {
-                    new Function(codes[i])();
-                } catch(e) {
-                    console.log(e.name, e.message);
-                }
-            }
-        });
-
-    }, false);
-
     var get = function(scripts) {
 
         var promises = [];
@@ -106,5 +66,46 @@
         return Promise.all(promises);
 
     }
+
+    // hook up
+    d.addEventListener('DOMContentLoaded', function(){
+
+        var data = [],
+            scripts = d.getElementsByTagName('script');
+
+        // check if local storage is supported
+        if (ls) {
+            var time = Math.floor(new Date().getTime() / 1000),
+                expire = ls.getItem('ls-expire'),
+                expire_in = self.getAttribute('data-expire') || 3600;  // expire in seconds
+            // clear if data is expired
+            if (null === expire || time > expire) {
+                ls.clear();
+                ls.setItem('ls-expire', parseInt(time) + parseInt(expire_in));
+            }
+        }
+
+        // process all script tags
+        for(var i in scripts) {
+            if (scripts[i].type == 'text/localscript') {
+                data.push({
+                    url : scripts[i].getAttribute('data-src') || null,
+                    code : scripts[i].innerHTML || null
+                });
+            }
+        }
+
+        // run
+        get(data).then(function(codes){
+            for(var i in codes) {
+                try {
+                    (0, eval)(codes[i]);
+                } catch(e) {
+                    console.log(e.name, e.message);
+                }
+            }
+        });
+
+    }, false);
 
 })(this);
